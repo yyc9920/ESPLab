2eacfcdd352e7eb181ca5fa6b4a5bcf226bdfa4f
yyc9920 <53812519+yyc9920@users.noreply.github.com>
2019-10-12 10:08:54 +0900

    Rename DFT3_2_2.m to DFT_3_2_2.m

--- DFT_3_2_2.m
+++ DFT_3_2_2.m
@@ -78,10 +78,6 @@ hold on;
 
 plot(f_hat, abs(Xk), 'ro ');
 
-f0 = linspace(-0.5, 0.5, 1024);
-
-X = 50*sinc(50*f0);
-
 n = 0:1:49;
 
 x = zeros(1, 50);
@@ -92,12 +88,21 @@ end
 
 [f_hat, Xk, N_mult] = NpointDFT(x);
 
+n1 = 0:1:1023;
+
+x1 = zeros(1, length(n1));
+
+for i=1:1:max(find(n1<50))
+    x1(i) = 1;
+end
+
+[f0, X] = myfun_SA(n1, x1);
+
 subplot(427);
 stem(n, x);
 
 subplot(428);
 plot(f0, abs(X));
-xlim([-0.5 0.5]);
 hold on;
 
 plot(f_hat, abs(Xk), 'ro ');
