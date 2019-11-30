a49e667f132a81b3984136a5eace61aaa1bd9fcf
yyc9920 <53812519+yyc9920@users.noreply.github.com>
2019-11-30 15:35:34 +0900

    Update IIR_3_2.m

--- IIR_3_2.m
+++ IIR_3_2.m
@@ -4,29 +4,30 @@ clc;
 [z p k] = buttap(7);
 [b, a] = zp2tf(z,p,k);
 
-[bt, at] = lp2bp(b, a, 6000/2, 2000);   % BPF
-[bt1, at1] = lp2hp(b, a, 4000);         % HPF
+[bt, at] = lp2bp(b, a, 3000*2*pi, 2000*2*pi);   % BPF
+[bt1, at1] = lp2hp(b, a, 4000*2*pi);         % HPF
 
-[numd, dend] = bilinear(bt, at, 8000);
-[numd1, dend1] = bilinear(bt1, at1, 8000);
+[numd, dend] = bilinear(bt, at, 50000);
+[numd1, dend1] = bilinear(bt1, at1, 50000);
 
-w = linspace(0,5500,1000);
+f = linspace(0,5500,110000);
+w = f*2*pi;
 ht = freqs(bt,at,w);
 ht1 = freqs(bt1,at1,w);
-hn = freqz(numd, dend, w/8000);
-hn1 = freqz(numd1, dend1, w/8000);
+hn = freqz(numd, dend, w./50000);
+hn1 = freqz(numd1, dend1, w./50000);
 
-plot(w, abs(ht), 'b','LineWidth',3);
+plot(f, abs(ht), 'b','LineWidth',3);
 grid on;
 hold on;
-ylabel('|H(w)|');
+ylabel('|H(f)|');
 xlabel('Frequency, f[Hz]');
 
-plot(w, abs(hn), 'g','LineWidth',3);
+plot(f, abs(hn), 'g','LineWidth',3);
 
-plot(w, abs(ht1), 'r','LineWidth',3);
+plot(f, abs(ht1), 'r','LineWidth',3);
 
-plot(w, abs(hn1), 'c','LineWidth',3);
+plot(f, abs(hn1), 'c','LineWidth',3);
 axis([1500 5500 0 1.4]);
 
 legend('Analog BPF', 'Digital BPF', 'Analog HPF', 'Digital HPF');
